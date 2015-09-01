e2d0f5a25dad7c5bd9be54f8da8f68bd881dac0f
reymwarfly <drew.carr0@gmail.com>
2015-09-01 14:26:33 -0500

    Drew's Code

--- Pilot/src/process.java
+++ Pilot/src/process.java
@@ -1,4 +1,58 @@
 
+import java.util.Scanner;
 public class process {
-
+	int xcord,ycord,zcord,angle;
+	Scanner input = new Scanner( System.in );
+	boolean done = false, finished = false;
+	while(done == false)
+	{
+		System.out.println("What would you like to do?");
+		
+		String in = input.nextLine();
+		if(in.equals("Change"))
+		{
+			
+		}
+		if(in.equals("Display"))
+		{
+			while(finished == false)
+			{
+				System.out.println("What would you like to do?");
+				String choice = input.nextLine();				
+				if(choice.equals("exit"))
+				{
+					break;
+				}
+			}
+		}
+		if(in.equals("exit"))
+		{
+			done = true;
+		}
+	}
+}
+}
+class Change
+{
+	public void changeAngle()
+	{
+		//send to server
+	}
+	public void goRight()
+	{
+		
+	}
+	public void goLeft()
+	{
+		
+	}
+	public void goUp()
+	{
+		
+	}
+	public void goDown()
+	{
+		
+	}
+	
 }
