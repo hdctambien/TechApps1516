b029a9d391df2a9c93983ef774379bc597431d8a
Justin Pierre <bobustin2@gmail.com>
2015-09-03 13:29:04 -0500

    Justin's First Commit

---
+++ GroundControl/src/spacegame/groundcontrol/GUI.java
@@ -0,0 +1,20 @@
+package spacegame.groundcontrol;
+
+import java.awt.Canvas;
+
+public class GUI extends Canvas implements Runnable
+{
+	private boolean isRunning = false;
+	
+	public GUI()
+	{
+		
+	}
+	
+	public void run() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ GroundControl/src/spacegame/groundcontrol/RunGroundControl.java
@@ -0,0 +1,15 @@
+package spacegame.groundcontrol;
+
+import java.awt.Canvas;
+
+
+public class RunGroundControl
+{
+	private static GUI gui;
+	public static void main(String[] args)
+	{
+		gui = new GUI();
+		gui.run();
+	}
+	
+}
