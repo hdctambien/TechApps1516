c2032f9e824cf5a470e47d47f3b298a2007385d1
Caleb Wilson <lodeous@gmail.com>
2015-09-03 14:55:00 -0500

    Generated javadoc for client and updated the client jar

--- GameClient/src/spacegame/client/BasicProtocol.java
+++ GameClient/src/spacegame/client/BasicProtocol.java
@@ -1,18 +1,34 @@
 package spacegame.client;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 
+/**
+ * This is a Basic implementation of AbstractProtocol that prints messages recieved from the server to the
+ * standard output (System.out). It also contains functionality for cleanly terminating the connection with the
+ * server.
+ * @author Caleb Wilson
+ */
 public class BasicProtocol extends AbstractProtocol {
 	
 	private InetAddress iaddress;
 	private int port;
 	
+	/**
+	 * Creates a BasicProtocol object to handle messages received by the Client.
+	 *  In order to start this protocol listening to the client for messages,
+	 * you need to create and start a Thread for this protocol with the code 'Thread t = new Thread(protocol);'
+	 * and 't.start();'.
+	 * @param client a spacegame.client.Client object that this protocol binds to
+	 */
 	public BasicProtocol(Client client){
 		super(client);
 		iaddress = client.getAddress();
 		port = client.getPort();
 	}
 	
+	/**
+	 * This implements the abstract method process from AbstractProtocol. Messages received from the client will
+	 * be displayed on the Standard Output (System.out).
+	 */
 	@Override
 	public void process(String command) {
 		if(command.equals("TERM")){
--- GameClient/src/spacegame/client/Client.java
+++ GameClient/src/spacegame/client/Client.java
@@ -8,6 +8,14 @@ import java.net.Socket;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+/**
+ * This class connects to a server at the specified IP Address and port. Create a thread for the client so that
+ * it will run its read loop. Whenever the read loop gets a message from the server, it transfers it to an 
+ * internal queue. The messages are made available through the methods 'boolean hasMessage()' and 
+ * 'String getNextMessage()'. You can also send messages back to the server using the client's method
+ * 'void sendMessage(String message)'
+ * @author Caleb Wilson
+ */
 public class Client implements Runnable{
 	
 	private InetAddress iaddress;
@@ -19,6 +27,14 @@ public class Client implements Runnable{
 	
 	private volatile boolean done;
 	
+	/**
+	 * Creates a new Client object that connects to the server at the given IP Address and port. Once the client
+	 * is created it should be put in a thread object to start its read loop with code like this 
+	 * 'Thread t = new Thread(client);' and 't.start();'
+	 * @param address the IP Address or host name of the server
+	 * @param port the port of the server
+	 * @throws IOException if the socket created is unable to connect to the server
+	 */
 	public Client(String address, int port) throws IOException{
 		iaddress = InetAddress.getByName(address);
 		this.port = port;
@@ -28,26 +44,43 @@ public class Client implements Runnable{
 		messageQueue = new LinkedBlockingQueue<String>();
 	}
 	
-	protected PrintWriter getPrintWriter(){
-		return writer;
-	}
-	
+	/**
+	 * A method that returns the IP Address of the server that the client is connected to
+	 * @return the IP Address of the server that the client is connected to
+	 */
 	public InetAddress getAddress(){
 		return iaddress;
 	}
 	
+	/**
+	 * A method that returns the port on the server that the client is connected to
+	 * @return the port on the server that the client is connected to
+	 */
 	public int getPort(){
 		return port;
 	}
 	
-	//TODO: Implement these methods!!!
+	/**
+	 * This method sends the given message to the server that the client is connected to
+	 * @param message the message String to be sent to the server
+	 */
 	public void sendMessage(String message){
 		writer.println(message);
 		writer.flush();
 	}
+	/**
+	 * This method returns true if the Client's message Queue has a message from the server and false if it 
+	 * does not contain any messages
+	 * @return whether or not the Client's message Queue has a message available
+	 */
 	public synchronized boolean hasMessage(){
 		return !messageQueue.isEmpty();
 	}
+	/**
+	 * This method returns the next message from the Client's message queue, if available.
+	 * If there is no available message, the String returned will be null.
+	 * @return the next message in the Client's queue from the server
+	 */
 	public synchronized String getNextMessage(){
 		return messageQueue.poll();
 	}
@@ -55,6 +88,14 @@ public class Client implements Runnable{
 		messageQueue.add(message);
 	}
 
+	/**
+	 * This method should be called by a Thread object. It loops indefinitely until the method 'stop()' is
+	 * called or until the client receives a command by the server to stop reading (then the server closes the
+	 * connection).
+	 * This loop reads line by line from the server and every line it reads is added as a String to the client's
+	 * internal message queue. These messages can be accessed via the methods 'boolean hasMessage()' and
+	 * 'String getNextMessage()'
+	 */
 	@Override
 	public void run() {
 		while(!done){
@@ -71,6 +112,11 @@ public class Client implements Runnable{
 		}		
 	}
 	
+	/**
+	 * This method stops the client read thread that is looping to receive messages from the server. 
+	 * Call this method when you would like to stop reading from the server.
+	 * Please note that the client itself may end its read loop if it is told by the server to exit.
+	 */
 	public void stop(){
 		done = true;
 	}
