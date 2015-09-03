70d41bf63b0fcf01fc364406603fcfc27678f66b
Justin Pierre <bobustin2@gmail.com>
2015-09-03 14:28:59 -0500

    Added gui and game threads to the Ground Controller
    added a game thread and a graphics thread to the Ground Controller, as
    well as basic window functionality.

--- GroundControl/src/spacegame/groundcontrol/RunGroundControl.java
+++ GroundControl/src/spacegame/groundcontrol/RunGroundControl.java
@@ -2,14 +2,14 @@ package spacegame.groundcontrol;
 
 import java.awt.Canvas;
 
-
 public class RunGroundControl
 {
-	private static GUI gui;
+	private static groundControlGame game;
 	public static void main(String[] args)
 	{
-		gui = new GUI();
-		gui.run();
+		game = new groundControlGame();
+		game.run();
+		System.out.println("Something should be happening now");
 	}
 	
 }
