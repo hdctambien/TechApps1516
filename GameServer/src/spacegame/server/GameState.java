30fcf924dc51e0b9131440ca4f3c76c9aae0cec6
Caleb Wilson <lodeous@gmail.com>
2015-09-11 08:47:38 -0500

    Added variables to GameState, shared batch files, and generated a jar file for the server

--- GameServer/src/spacegame/server/GameState.java
+++ GameServer/src/spacegame/server/GameState.java
@@ -3,43 +3,84 @@ package spacegame.server;
 public class GameState {
 
 	public static final String BUTTON_STATUS = "buttonStatus";
+	public static final String ROCKET_VELOCITY = "rocketVelocity";
+	public static final String ROCKET_HEADING = "rocketHeading";
+	public static final String ROCKET_POS = "rocketPos";
 	
-	private boolean buttonStatus;
+	private volatile boolean buttonStatus;
+	private volatile double rocketVelocity; // m/s
+	private volatile double rocketHeading;  // radians
+	private volatile double rocketPos;      //Position (m?)
 	
 	public GameState(){
 		buttonStatus = false;
-		
+		rocketVelocity = 0.0;
+		rocketHeading = Math.PI/2;
+		rocketPos = 0.0;
 	}
 	
-	public boolean getButtonStatus(){
+	public synchronized boolean getButtonStatus(){
 		return buttonStatus;
 	}
-	public void setButtonStatus(boolean status){
+	public synchronized void setButtonStatus(boolean status){
 		buttonStatus = status;
 	}
-	
-	public void doGet(String var, ClientInfo info, Request r){
+	public synchronized double getRocketVelocity(){
+		return rocketVelocity;
+	}
+	public synchronized void setRocketVelocity(double velocity){
+		rocketVelocity = velocity;
+	}
+	public synchronized double getRocketHeading(){
+		return rocketHeading;
+	}
+	public synchronized void setRocketHeading(double heading){
+		rocketHeading = heading;
+	}
+	public synchronized double getRocketPosition(){
+		return rocketPos;
+	}
+	public synchronized void setRocketPosition(double position){
+		rocketPos = position;
+	}
+	public synchronized void doGet(String var, ClientInfo info, Request r){
 		switch(var){
 			case BUTTON_STATUS:
 				info.sendMessage("set buttonStatus "+buttonStatus);
+				break;
+			case ROCKET_VELOCITY:
+				info.sendMessage("set rocketVelocity "+rocketVelocity);
+				break;
+			case ROCKET_HEADING:
+				info.sendMessage("set rocketHeading "+rocketHeading);
+				break;
+			case ROCKET_POS:
+				info.sendMessage("set rocketPos "+rocketPos);
 				break;
 			default:
 				r.reply("UNK");
 				break;
 		}
 	}
-	public void doSet(String var, String val, ClientInfo info, Request r){
+	public synchronized void doSet(String var, String val, ClientInfo info, Request r){
 		switch(var){
-		case BUTTON_STATUS:
-			if(tryBooleanParse(var,val)){
-				r.reply("OK");
-			}else{
-				r.reply("ERR "+SpacegameNetworkProtocol.ERR_PARSE_VAL);
-			}
-			break;
-		default:
-			r.reply("UNK");
-			break;
+			case BUTTON_STATUS:
+				if(tryBooleanParse(var,val)){
+					r.reply("OK");
+				}else{
+					r.reply("ERR "+SpacegameNetworkProtocol.ERR_PARSE_VAL);
+				}
+				break;
+			case ROCKET_VELOCITY: case ROCKET_HEADING: case ROCKET_POS:
+				if(tryDoubleParse(var,val)){
+					r.reply("OK");
+				}else{
+					r.reply("ERR "+SpacegameNetworkProtocol.ERR_PARSE_VAL);
+				}
+				break;
+			default:
+				r.reply("UNK");
+				break;
 		}
 	}
 	public boolean tryBooleanParse(String var, String val){
@@ -53,5 +94,26 @@ public class GameState {
 				break;
 		}
 		return true;
+	}
+	public boolean tryDoubleParse(String var, String val){
+		boolean success = true;
+		double d = Double.NaN;
+		try{
+			d = Double.parseDouble(val);
+			switch(var){
+				case ROCKET_VELOCITY:
+					rocketVelocity = d;
+					break;
+				case ROCKET_HEADING:
+					rocketHeading = d;
+					break;
+				case ROCKET_POS:
+					rocketPos = d;
+					break;
+			}
+		}catch(NumberFormatException e){
+			success = false;
+		}		
+		return success;
 	}
 }
---
+++ Lib/runServer.bat
@@ -0,0 +1,1 @@
+java -jar GameServer.jar
