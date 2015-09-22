9e7f7084641bbf709457401c2dc5b61f81f4535e
AndrewHosang <ajhosang2@gmail.com>
2015-09-22 13:46:52 -0500

    Something is broken but still works somehow
    Power sliders won't work and won't show up but somehow original
    components are still visible. Broken

--- Engineer/src/spacegame/engineer/EngineerGame.java
+++ Engineer/src/spacegame/engineer/EngineerGame.java
@@ -16,10 +16,7 @@ public class EngineerGame implements Runnable
 	Client eClient;
 	EngineerProtocol eProtocol;
 	String iaddress = "10.11.1.110";
-	//String iaddress = "192.168.1.89";
 	int port = 8080;
-	JButton exit;
-	JButton get;
 	
 	private Thread protocolThread;
 	private Thread clientThread;
@@ -39,12 +36,36 @@ public class EngineerGame implements Runnable
 		eProtocol = new EngineerProtocol(eClient, this, gui);
 		protocolThread = new Thread(eProtocol);
 		protocolThread.start();
-		//run();
 	}
 	
 	public void Throttle(int throt)
 	{
-		eClient.sendMessage("set throttle "+Integer.toString(throt));
+		eClient.sendMessage("set throttle " + Integer.toString(throt));
+	}
+	
+	public void powerDist(int power, int pS, int pF, int pC, int pG)
+	{
+		
+	}
+	
+	public void powerShield(int pShield)
+	{
+		eClient.sendMessage("set powerShield " + Integer.toString(pShield));
+	}
+	
+	public void powerFuel(int pFuel)
+	{
+		eClient.sendMessage("set powerFuel " + Integer.toString(pFuel));
+	}
+	
+	public void powerComms(int pComms)
+	{
+		eClient.sendMessage("set powerComms " + Integer.toString(pComms));
+	}
+	
+	public void powerGuns(int pGuns)
+	{
+		eClient.sendMessage("set powerGuns " + Integer.toString(pGuns));
 	}
 	
 	public void run()
