d3be20aec252e785f671f89fa3ebd8bea36c1e45
Caleb Wilson <lodeous@gmail.com>
2015-09-22 13:22:41 -0500

    Added protocol functionality to components

--- GameClient/src/spacegame/map/Component.java
+++ GameClient/src/spacegame/map/Component.java
@@ -5,5 +5,8 @@ public abstract class Component {
 	public abstract void update(long timeElapsed);
 	public abstract void sync(Component c);
 	public abstract Component clone();
+	public abstract boolean hasVariable(String varname);
+	public abstract String getVariable(String varname);
+	public abstract boolean setVariable(String varname, String value);
 	
 }
