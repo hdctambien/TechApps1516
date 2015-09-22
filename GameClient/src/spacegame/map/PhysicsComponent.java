394f489646bb4356b61b24c9de45a4659e1e1f03
Justin Pierre <bobustin2@gmail.com>
2015-09-22 13:46:32 -0500

    Added get and set methods for PhysicsComponent

--- GameClient/src/spacegame/map/PhysicsComponent.java
+++ GameClient/src/spacegame/map/PhysicsComponent.java
@@ -29,15 +29,66 @@ public class PhysicsComponent extends Component
 
 	
 	@Override
-	public void sync(Component c) {
+	public void sync(Component c) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Component clone() {
+	public Component clone() 
+	{
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+	@Override
+	public boolean hasVariable(String varname) 
+	{
+		switch(varname)
+		{
+			case "velocityX":
+			case "velocityY":
+			case "heading":
+			case "posX":
+			case "posY":
+			case "throttle":
+				return true;
+			default: return false;
+		}
+	}
+
+
+	@Override
+	public String getVariable(String varname) 
+	{
+		switch(varname)
+		{
+			case "velocityX": return Double.toString(xVel);
+			case "velocityY": return Double.toString(yVel);
+			case "heading": return Double.toString(heading);
+			case "posX": return Double.toString(xPos);
+			case "posY": return Double.toString(yPos);
+			case "throttle": return Double.toString(throttle);
+			default: return null;
+		}
+	}
+
+
+	@Override
+	public boolean setVariable(String varname, String value) 
+	{
+		switch(varname)
+		{
+			case "velocityX": xVel = Double.parseDouble(value); return true;
+			case "velocityY": yVel = Double.parseDouble(value); return true;
+			case "heading": heading = Double.parseDouble(value); return true;
+			case "posX": xPos = Double.parseDouble(value); return true;
+			case "posY": yPos = Double.parseDouble(value); return true;
+			case "throttle": throttle = Double.parseDouble(value); return true;
+			default: return false;
+		}
 	}
 	
 }
