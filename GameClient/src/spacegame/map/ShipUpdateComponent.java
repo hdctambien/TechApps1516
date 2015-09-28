3b1c227eda9e7e42d83e25d7866306a3bae2fdbc
Caleb Wilson <lodeous@gmail.com>
2015-09-28 13:44:54 -0500

    added ship update component

---
+++ GameClient/src/spacegame/map/ShipUpdateComponent.java
@@ -0,0 +1,36 @@
+package spacegame.map;
+
+public class ShipUpdateComponent extends UpdateComponent{
+
+	private PositionComponent position;
+	private PhysicsComponent physics;
+	private PowerComponent power;
+	private FuelComponent fuel;
+	
+	public ShipUpdateComponent(Entity entity) {
+		super(entity);
+		
+	}
+
+	@Override
+	public void update(long timeElapsed) {
+		power.calculatePower();
+		if(!fuel.isFull()){
+			fuel.regenFuel(timeElapsed);
+		}
+		fuel.consumeFuel(timeElapsed);
+		if(fuel.checkThrottle()){
+			physics.changeAcceleration();
+		}
+		physics.accelerate(timeElapsed);
+		physics.move(timeElapsed);
+	}
+
+	@Override
+	public Component clone(Entity entity) {
+		return new ShipUpdateComponent(entity);
+	}
+
+
+
+}
