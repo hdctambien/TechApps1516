f08c57c311436e851d055f2578f52885c39edf41
reymwarfly <drew.carr0@gmail.com>
2015-10-16 13:18:34 -0500

    Heading Works(sorta)

--- Map/src/MapPanel.java
+++ Map/src/MapPanel.java
@@ -14,24 +14,19 @@ import javax.swing.JViewport;
 
 import spacegame.map.MapAction;
 
-public class MapPanel{
+public class MapPanel extends JFrame{
 	public static int xCord, yCord;
+	public static double heading = 45;
 	
 	
-	public static JFrame frame = new JFrame();
-	public static JPanel mapPanel;
-	public static JViewport view;
-	public static JLabel ship;
-	public static Point point;
-	public static MapComponent map;
+	public  static JFrame frame = new JFrame();
+	public  static JPanel mapPanel;
+	public  static MapComponent map;
 	public static void mapPanel()
 	{
-		Scanner input = new Scanner( System.in );
-		boolean running = true;
-		
-		final MapClient pilot = new MapClient();
+	/*	final MapClient pilot = new MapClient();
 		pilot.setup();
-		pilot.subscribe();
+		pilot.subscribe();*/
 		
 	
 		mapPanel = new JPanel();
@@ -39,44 +34,19 @@ public class MapPanel{
 		
 		map = new MapComponent();
 		map.setPreferredSize(new Dimension(1200,600));
-/*		JScrollPane pane = new JScrollPane();
-		pane.setViewport(view);
-		pane.setVisible(true);*/
-		
-		
-	/*	point = new Point();
-		point.setLocation(100,100);
-		
-		Dimension dimension = new Dimension();
-		dimension.setSize(800, 800);
-		Dimension dimension1 = new Dimension();
-		dimension1.setSize(100,100);
-		
-		pane.setPreferredSize(dimension);*/
-	
 
-/*		view = new JViewport();
-		view.setView(mapPanel);
-		view.setPreferredSize(new Dimension(100,100));
-		view.setExtentSize(dimension);
-		view.setOpaque(false);
-		view.setViewSize(dimension1);
-		view.setViewPosition(point);
-		view.setVisible(true); */
-		
-		//frame.setLayout(null);
-		frame.add(mapPanel, BorderLayout.NORTH);
-		frame.add(map, BorderLayout.SOUTH);
+//		frame.add(mapPanel, BorderLayout.NORTH);
+		frame.add(map, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800,800);
 		frame.setVisible(true);
 		
+	
+		xCord = 200;
+		yCord = 200;
+		map.setHeading(heading);
 		
 		run();
-		
-	//	choice = input.nextLine();
-	//	ship.setBounds(500, 500, 50, 50);
-	//	frame.revalidate();
 		
 	}
 	public static void main(String[] args)
@@ -87,9 +57,18 @@ public class MapPanel{
 	{
 		while(true)
 		{
+			try
+			{
+				Thread.sleep(1);
+			}
+			catch(InterruptedException ex)
+			{
+				Thread.currentThread().interrupt();
+			}
+			map.setHeading(0);
 			map.setPosition(xCord, yCord);
-			map.repaint();
 			frame.revalidate();
+			map.repaint();
 		}
 	}
 
@@ -102,5 +81,9 @@ public class MapPanel{
 	{
 		xCord = posX;
 		System.out.println("set x to: "+ posX);
+	}
+	public static void setHeading(double h)
+	{
+		heading = h;
 	}
 }
