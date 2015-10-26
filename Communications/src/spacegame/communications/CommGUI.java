4b094978cc9bc24964201f749d82aa1b03dee464
averyjennings <averyjennings96@gmail.com>
2015-10-26 13:36:50 -0500

    q

--- Communications/src/spacegame/communications/CommGUI.java
+++ Communications/src/spacegame/communications/CommGUI.java
@@ -10,8 +10,11 @@ import java.awt.Graphics;
 
 
 
+
 import spacegame.client.*;
 import spacegame.gui.*;
+import spacegame.map.GameMap;
+import spacegame.mapgui.*;
 
 public class CommGUI extends JPanel implements Runnable {
 
@@ -20,30 +23,33 @@ public class CommGUI extends JPanel implements Runnable {
     private static CommGame game;
     private static Client client;
     private MouseListener m;
-  //  public JPanel panel = new JPanel();
-    private boolean buttonStatus = false;
-    private boolean buttonUpdated = true;
     public JFrame windowFrame;
     private HeadingDial headingDial = new HeadingDial();
     public Container contentPane;
 
-    public CommGUI(CommGame game, Client c){
-        this.game=game;
+    public CommGUI(CommGame game, Client c, MapPanel map){
+        
+    	windowFrame.setVisible(true);
+   		windowFrame.setSize(new Dimension(1600,900));
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    	
+    	this.game=game;
         client=c;
 
-
-    
-        
         windowFrame = new JFrame();
         this.client = c;
 
 
-        //gui test
+        
 //        JButton cancelButton = new JButton("cancel");
-//        JButton setButton = new JButton("set");        		
+//        JButton setButton = new JButton("set");       	
+     
+       
+        
 //        JScrollPane listScroller = new JScrollPane();
-//        listScroller.setPreferredSize(new Dimension(250, 80));
-//        listScroller.setAlignmentX(LEFT_ALIGNMENT);
+       
+//        map.setAlignmentX(LEFT_ALIGNMENT);
 //        JPanel listPane = new JPanel();
 //        listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
 //        JLabel label = new JLabel("sup bruh");
@@ -51,33 +57,33 @@ public class CommGUI extends JPanel implements Runnable {
 //        listPane.add(Box.createRigidArea(new Dimension(0,5)));
 //        listPane.add(listScroller);
 //        listPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-        	JPanel buttonPane = new JPanel();
-        	buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
-        	buttonPane.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
-        	buttonPane.add(Box.createHorizontalGlue());
+        
+        
+//        	JPanel buttonPane = new JPanel();
+//        	buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
+//        	buttonPane.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+//        	buttonPane.add(Box.createHorizontalGlue());
+        	
+        	
 //        buttonPane.add(cancelButton);
 //        buttonPane.add(Box.createRigidArea(new Dimension(10, 0)));
 //        buttonPane.add(setButton);
         
-       
 
        
         headingDial.setRadius(100);
         
         
         contentPane = windowFrame.getContentPane();
-  //      contentPane.add(listPane, BorderLayout.CENTER);
+        contentPane.add(map, BorderLayout.CENTER);
         
         contentPane.add(headingDial, BorderLayout.EAST);
-        contentPane.add(buttonPane, BorderLayout.PAGE_END);
+//        contentPane.add(buttonPane, BorderLayout.PAGE_END);
         contentPane.addMouseListener(mouse);
         
         
         
         
-        windowFrame.setVisible(true);
-		windowFrame.setPreferredSize(new Dimension(1000,700));
-        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      
         windowFrame.pack();
      
@@ -108,14 +114,14 @@ public class CommGUI extends JPanel implements Runnable {
     		if(mouseClick)
     		{
     			i+=.0001;
-    			headingDial.setRadius((int)i+1);
+    		//	headingDial.setRadius((int)i+1);
     			System.out.println("test");
             	headingDial.setHeading(i);
             	windowFrame.repaint();
             	
     		}
     		
-    		windowFrame.repaint();
+    	//	windowFrame.repaint();
     	}
     	
     }
